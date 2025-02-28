c8c37553c8687118d3d2af3151f8047a54fa00a8
DeltaV <Tristantdm1@outlook.com>
2025-02-27 20:10:00 -0500

    givt

--- new-game-project/Scenes/Province.gd
+++ new-game-project/Scenes/Province.gd
@@ -1,18 +1,50 @@
-extends Sprite2D
+extends TextureButton
 
-@export var Neighbors : Array[Node2D]
+@export var Unit = preload ("res://Unit.tscn")
+
+@export var Neighbors : Array[TextureButton]
+
+@export var Swiss : bool
+
+@export var occupied : bool
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	# Get the image from the texture normal
+	var image = texture_normal.get_image()
+	# Create the BitMap
+	var bitmap = BitMap.new()
+	# Fill it from the image alpha
+	bitmap.create_from_image_alpha(image)
+	# Assign it to the mask
+	texture_click_mask = bitmap
+	
+	if occupied:
+		SpawnUnit()
+	
+			
 	pass # Replace with function body.
 
 
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if  event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		print("lol it works")
+func SpawnUnit():
+	var instance = Unit.instantiate()
+	add_child(instance)
+	instance.global_position = global_position + Vector2(size.x / 2, size.y / 2)
+	if not Swiss:
+		instance.modulate = Color.hex(0x6192ffff)
+	else:
+		instance.modulate = Color.hex(0xfc5a00ff)
+	#instance.reparent()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
+
+
+func _on_pressed() -> void:
+	modulate = Color(0, 0, 0)
+	pass # Replace with function body.
